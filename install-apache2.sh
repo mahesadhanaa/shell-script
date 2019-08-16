7c2b7ff73eb07e3e5d7d8379a4cfe1a754457dfe
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-16 23:15:46 +0700

    add pesbuk

--- install-apache2.sh
+++ install-apache2.sh
@@ -6,3 +6,10 @@ sudo apt-get install -y apache2 php php-mysql
 #echo "Melakukan Installasi Database Server"
 #sudo apt-get install -y mysql-server
 echo "Installasi Selesai"
+
+apt-get install unzip
+wget https://github.com/sdcilsy/sosial-media/archive/master.zip
+unzip master.zip
+sudo rm /var/www/html/*
+sudo rm -R /var/www/html/*
+sudo mv sosial-media-master/* /var/www/html
