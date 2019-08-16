b72d5350138371c271e5c59e00e36ca7aab9399e
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-16 22:04:12 +0700

    Update install-apache2.sh

--- install-apache2.sh
+++ install-apache2.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-jawaban="Y"
-read -p "Apakah kamu yakin akan menginstall webserver ? (Y/n) " pilih;
-if [ $pilih == $jawaban ];
-then
-        echo "Menyiapkan Installasi Web server"
-        sudo apt-get update
-        echo "Melakukan Installasi Webserver"
-        sudo apt-get install -y apache2 php php-mysql
-        #echo "Melakukan Installasi Database Server"
-        #sudo apt-get install -y mysql-server
-        echo "Installasi Selesai"
-        exit 0
-else
-        echo "Installasi dibatalkan"
-        exit 1
-fi
+echo "Menyiapkan Installasi Web server"
+sudo apt-get update
+echo "Melakukan Installasi Webserver"
+sudo apt-get install -y apache2 php php-mysql
+#echo "Melakukan Installasi Database Server"
+#sudo apt-get install -y mysql-server
+echo "Installasi Selesai"
