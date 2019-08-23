ba66717963c01166370e65c8734c9682a5494ba1
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-23 21:03:41 +0700

    add file ec2 script

---
+++ ec2-script.sh
@@ -0,0 +1,5 @@
+aws ec2 create-security-group --group-name mhs-sg --description "My security group"
+aws ec2 create-security-group --group-name EC2SecurityGroup --description "Security Group for EC2 instances to allow port 22"
+aws ec2 authorize-security-group-ingress --group-name EC2SecurityGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 describe-security-groups --group-names EC2SecurityGroup
+aws ec2 run-instances --image-id ami-5ec1673e --key-name MyKey --security-groups EC2SecurityGroup --instance-type t2.micro --placement AvailabilityZone=us-east-2a --block-device-mappings DeviceName=/dev/sdh,Ebs={VolumeSize=100} --count 2 --user-data file://vm-script.txt
