b31f91573a26f6211bdf18c1e94d8ea18f914048
mahesadhanaa <mahesadhana91@gmail.com>
2019-10-24 20:59:42 +0700

    test

--- install-apache2.sh
+++ install-apache2.sh
@@ -5,7 +5,7 @@ echo "Melakukan Installasi Webserver"
 #sudo apt-get install -y apache2 php php-mysql
 echo "Melakukan Installasi Database Server"
 #sudo apt-get install -y mysql-server
-echo "Installasi Selesai"
+echo "Installasi Selesaii"
 
 #git clone https://github.com/mahesadhanaa/sosial-media.git
 # sudo rm /var/www/html/*
